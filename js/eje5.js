5bccc53c8baa5338de68af3aac52409d50a715be
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-03-22 03:17:03 -0300

    comence el eje5

---
+++ js/eje5.js
@@ -0,0 +1,21 @@
+/**5- Crea una clase llamada Persona que siga las siguientes condiciones:
+Sus propiedades son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura, año de nacimiento.
+ Si quieres añadir alguna propiedad extra puedes hacerlo.
+Los métodos que se debe poder utilizar  son:
+mostrarGeneracion: este método debe mostrar un mensaje indicando a qué generación pertenece la persona creada y
+cual es el rasgo característico de esta generación.
+ */
+
+class Persona {
+    constructor(nombre, edad, dni, sexo, peso, altura, nacimiento) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+        this.nacimiento = nacimiento;
+    }
+    //metodos
+    
+}
